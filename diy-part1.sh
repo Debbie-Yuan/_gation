3b2684523f010abc1c20b1c928d6c24957229ce2
Debbie-Yuan <34805722+Debbie-Yuan@users.noreply.github.com>
2023-03-13 14:57:16 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,7 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+git clone https://github.com/vernesong/OpenClash.git package/
+cd OpenClash
+mv luci-app-openclash ../
